280a1222234f4d95c5a363695b99e1e7c5918bb5
Derek Townsend <derekjavon2@gmail.com>
2020-05-01 23:52:37 -0500

    Controllers complete and serializers work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://antisocial202.herokuapp.com/'
+    origins 'antisocial202.herokuapp.com'
 
     resource '*',
       headers: :any,
